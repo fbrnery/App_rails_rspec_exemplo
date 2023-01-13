9e7c18c2f5e463733e22ce1fb75e1a19868dc0a9
Fabricio Miranda <fbrnery@gmail.com>
2023-01-13 07:07:59 -0300

    Criei uma features(rspec)

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature do
   scenario 'Mostra mensagem de Bem-Vindo' do
-    visit ('/')
+    visit (root_path)
       expect(page).to have_content('Bem-vindo')
+  end
+
+  scenario 'Verificar o link Cadastro de Clientes' do
+    visit(root_path)
+    #expect(page).to have_selector('ul li a')
+    #expect(page).to have_link('Cadastro de Clientes')
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
